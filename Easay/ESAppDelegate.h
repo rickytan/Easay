0b585b43ef4f61d56827112832a9345965aa2f8d
rickytan <ricky.tan.xin@gmail.com>
2014-04-04 02:05:46 +0800

    Initial Commit

---
+++ Easay/ESAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ESAppDelegate.h
+//  Easay
+//
+//  Created by ricky on 14-4-4.
+//  Copyright (c) 2014年 ricky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ESAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EasayTests/EasayTests.m
@@ -0,0 +1,34 @@
+//
+//  EasayTests.m
+//  EasayTests
+//
+//  Created by ricky on 14-4-4.
+//  Copyright (c) 2014年 ricky. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EasayTests : XCTestCase
+
+@end
+
+@implementation EasayTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
