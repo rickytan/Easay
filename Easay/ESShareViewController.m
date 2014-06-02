9cd6f41fb822aec70f4272b8f6d1686c9e681510
rickytan <ricky.tan.xin@gmail.com>
2014-06-03 04:30:03 +0800

    share

--- Easay/ESShareViewController.m
+++ Easay/ESShareViewController.m
@@ -124,18 +124,26 @@
 @property (nonatomic, assign) IBOutlet UIButton * editButton;
 @property (nonatomic, assign) IBOutlet UIButton * addButton;
 @property (nonatomic, assign) IBOutlet UIButton * weibo, * wechat, * path;
-@property (nonatomic, strong) IBOutlet SKView * balloonView;
 
 @property (nonatomic, strong) UIDynamicAnimator * animator;
 @property (nonatomic, strong) UICollisionBehavior * collision;
 @property (nonatomic, strong) UIGravityBehavior * gravity;
-@property (nonatomic, assign) SKNode * mainBalloon;
-@property (nonatomic, strong) ESScene * scene;
+@property (nonatomic, strong) UISnapBehavior * snap;
 @property (nonatomic, strong) NSMutableArray * balloons;
+@property (nonatomic, strong) CAShapeLayer * shape;
+@property (nonatomic, strong) CADisplayLink * displayLink;
+
+@property (nonatomic, assign) UIView * selectedBalloon;
 - (IBAction)onEdit:(id)sender;
 - (IBAction)onCamera:(id)sender;
 - (IBAction)onAdd:(id)sender;
 @end
+
+static CGPoint pos[] = {
+    {160, 568 - 150},
+    {90, 568 - 100},
+    {217, 568 - 80}
+};
 
 @implementation ESShareViewController
 
@@ -159,10 +167,30 @@
     self.collision = [[UICollisionBehavior alloc] init];
     self.gravity = [[UIGravityBehavior alloc] init];
 
-    self.scene = [ESScene sceneWithSize:self.balloonView.bounds.size];
-    [self.balloonView presentScene:self.scene];
-    self.balloonView.showsFPS = YES;
-    self.balloonView.showsNodeCount = YES;
+    self.shape = [CAShapeLayer layer];
+    self.shape.strokeColor = [UIColor whiteColor].CGColor;
+    self.shape.lineWidth = 1.0;
+    [self.view.layer insertSublayer:self.shape
+                            atIndex:0];
+
+    [self onAdd:nil];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    self.displayLink = [CADisplayLink displayLinkWithTarget:self
+                                                   selector:@selector(onDisplayLink:)];
+    [self.displayLink addToRunLoop:[NSRunLoop mainRunLoop]
+                           forMode:NSDefaultRunLoopMode];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [self.displayLink removeFromRunLoop:[NSRunLoop mainRunLoop]
+                                forMode:NSDefaultRunLoopMode];
+    self.displayLink = nil;
 }
 
 - (void)didReceiveMemoryWarning
@@ -171,14 +199,49 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)onDisplayLink:(CADisplayLink *)displayLink
+{
+    [self update:displayLink.duration];
+}
+
+- (void)update:(CGFloat)dt
+{
+    if (self.balloons.count == 0)
+        return;
+
+
+
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    for (UIView *view in self.balloons) {
+        if (view.isHidden)
+            continue;
+        [path moveToPoint:CGPointMake(CGRectGetMidX(self.view.bounds), CGRectGetMaxY(self.view.bounds))];
+        [path addLineToPoint:view.center];
+    }
+    self.shape.path = path.CGPath;
+}
+
+- (NSMutableArray *)balloons
+{
+    if (!_balloons) {
+        _balloons = [[NSMutableArray alloc] initWithCapacity:3];
+    }
+    return _balloons;
+}
+
 #pragma mark - Action
 
 - (IBAction)onAdd:(id)sender
 {
-    [self.scene makeBalloon];
-    if (self.scene.balloons.count >= 3) {
+    UIImageView *image = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"balloon"]];
+    image.center = self.view.center;
+    [self.view addSubview:image];
+    UISnapBehavior *snap = [[UISnapBehavior alloc] initWithItem:image snapToPoint:pos[self.balloons.count]];
+    [self.animator addBehavior:snap];
+    [self.balloons addObject:image];
+
+    if (self.balloons.count >= 3)
         self.addButton.hidden = YES;
-    }
 }
 
 - (IBAction)onEdit:(id)sender
@@ -208,15 +271,46 @@
     switch (pan.state) {
         case UIGestureRecognizerStateBegan:
         {
-            
+            for (UIView *v in self.balloons) {
+                if (CGRectContainsPoint(v.frame, [pan locationInView:self.view])) {
+                    self.selectedBalloon = v;
+                    break;
+                }
+            }
+            self.selectedBalloon.center = [pan locationInView:self.view];
         }
             break;
         case UIGestureRecognizerStateChanged:
-            pan.view.center = [pan locationInView:self.view];
+            self.selectedBalloon.center = [pan locationInView:self.view];
+            if (!self.wechat.isSelected)
+            self.wechat.highlighted = CGRectContainsPoint(self.wechat.frame, self.selectedBalloon.center);
+            if (!self.weibo.isSelected)
+            self.weibo.highlighted = CGRectContainsPoint(self.weibo.frame, self.selectedBalloon.center);
+            if (!self.path.isSelected)
+            self.path.highlighted = CGRectContainsPoint(self.path.frame, self.selectedBalloon.center);
             break;
         case UIGestureRecognizerStateEnded:
         case UIGestureRecognizerStateCancelled:
+        {
+            [self.animator updateItemUsingCurrentState:self.selectedBalloon];
+            if (self.wechat.isHighlighted ||
+                self.weibo.isHighlighted ||
+                self.path.isHighlighted) {
+                self.selectedBalloon.hidden = YES;
+            }
+            self.selectedBalloon = nil;
 
+            if (!self.weibo.isSelected)
+            self.weibo.selected = self.weibo.highlighted;
+            if (!self.wechat.isSelected)
+            self.wechat.selected = self.wechat.highlighted;
+            if (!self.path.isSelected)
+            self.path.selected = self.path.highlighted;
+
+            self.weibo.highlighted = NO;
+            self.wechat.highlighted = NO;
+            self.path.highlighted = NO;
+        }
             break;
         default:
             break;
